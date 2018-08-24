7c43a755fc8b93bfab1b9ae851d9a8a00dc3fc1e
Valentin Schukin <hekpoxuoypr@gmail.com>
2018-08-24 20:50:58 +0700

    init commit

---
+++ src/main/java/com/droptable/koronapayservice/KoronapayserviceApplication.java
@@ -0,0 +1,12 @@
+package com.droptable.koronapayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoronapayserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoronapayserviceApplication.class, args);
+    }
+}
---
+++ src/test/java/com/droptable/koronapayservice/KoronapayserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.droptable.koronapayservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KoronapayserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
