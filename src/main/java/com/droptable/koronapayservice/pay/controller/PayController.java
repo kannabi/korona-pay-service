aa300736b323ce1003864910eb234542cf3af35e
Valentin Schukin <hekpoxuoypr@gmail.com>
2018-08-25 14:52:11 +0700

    feat(docs): add swagger for pay controller

--- src/main/java/com/droptable/koronapayservice/pay/controller/PayController.java
+++ src/main/java/com/droptable/koronapayservice/pay/controller/PayController.java
@@ -2,6 +2,8 @@ package com.droptable.koronapayservice.pay.controller;
 
 import com.droptable.koronapayservice.pay.dao.Response;
 import com.droptable.koronapayservice.pay.service.PayService;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 
-@RestController
+@RestController()
+@RequestMapping("/pay")
 public class PayController {
 
     @Autowired
@@ -19,13 +22,29 @@ public class PayController {
 
     @RequestMapping(value = {"/main_pay"}, method = RequestMethod.POST)
     @ResponseBody
-    public Response processPay(String restaurantId, BigDecimal sum) {
+    @ApiOperation("Process main pay")
+    public Response processPay(
+            @ApiParam("Client's id")
+                    String userId,
+            @ApiParam("Just a dummy string that represent a restaurant account bill")
+                    String restaurantId,
+            @ApiParam("Sum of the order")
+                    BigDecimal sum
+    ) {
         return new Response(HttpStatus.OK.value(), "Pay successful");
     }
 
     @RequestMapping(value = {"/tip_pay"}, method = RequestMethod.POST)
     @ResponseBody
-    public Response processTip(String userId, String waiterId, BigDecimal sum) {
+    @ApiOperation("Process tips for waiter")
+    public Response processTip(
+            @ApiParam("Client's id")
+                    String userId,
+            @ApiParam("Waiter's id")
+                    String waiterId,
+            @ApiParam("Sum of a tip to the waiter")
+                    BigDecimal sum
+    ) {
         payService.processTip(userId, waiterId, sum);
         return new Response(HttpStatus.OK.value(), "Tip has been processed");
     }
